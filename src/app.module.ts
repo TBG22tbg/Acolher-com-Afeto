9b854597846a4729c32d41794cb47528801a5532
TARCISIO BARBOSA GONCALVES <tarcisio.bgoncalves@senacsp.edu.br>
2026-09-10 22:13:38 -0300

    Ajuste no app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
-import { FormularioUsuarioModule } from './formulario_usuario/formulario_usuario.module';
 import { FormProfissionalModule } from './form_profissional/form_profissional.module';
 import { FormUsuarioModule } from './form_usuario/form_usuario.module';
 
 @Module({
-  imports: [DatabaseModule, FormularioUsuarioModule, FormProfissionalModule, FormUsuarioModule],
+  imports: [DatabaseModule, FormProfissionalModule, FormUsuarioModule],
   controllers: [],
   providers: [],
 })

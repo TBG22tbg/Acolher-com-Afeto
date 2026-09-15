a78694fa00139fb9e0649a8d131010efed1183e3
TARCISIO BARBOSA GONCALVES <tarcisio.bgoncalves@senacsp.edu.br>
2026-09-14 21:42:25 -0300

    Nova atualização

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { ServeStaticModule} from '@nestjs/serve-static';
+import { join } from "path";
+
 import { DatabaseModule } from './database/database.module';
 import { FormUsuarioModule } from './form_usuario/form_usuario.module';
 
@@ -12,11 +15,13 @@ import { FormUsuarioModule } from './form_usuario/form_usuario.module';
         ConfigModule.forRoot({
             isGlobal: true,
         }),
+
+        ServeStaticModule.forRoot({
+            rootPath:join(__dirname,'..','public'),
+        }),
         DatabaseModule,
         FormUsuarioModule,
     ],
-    controllers: [],
-    providers: [],
 
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -28,6 +28,9 @@ async function bootstrap() {
 
   app.enableCors()
   
+  // const port = process.env.PORT ?? 3000;
+  // await app.listen(port, '0.0.0.0');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
